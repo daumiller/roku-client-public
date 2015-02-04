daumiller/roku-client-public

commit 1e3e51f7351028fe4bc2baa2172854ae25d77298

Initial TextArea component and implementation. #203
Implemented for the following:
 * Artist Biography
 * Album Review
 * Preplay Summary
 * Preplay Context Summary (show/season)

Fixes:
 * Scrollbar placement and height oddities

Plex2d/source/components/Label.brs
@@ -14,6 +14,7 @@ function LabelClass() as object
 
         obj.WrapText = labelWrapText
         obj.TruncateText = labelTruncateText
+        obj.GetAllLines = labelGetAllLines
         obj.MaxLineLength = labelMaxLineLength
 
         m.LabelClass = obj
@@ -119,17 +120,21 @@ sub labelSetColor(fgColor as integer, bgColor=invalid as dynamic)
     end if
 end sub
 
-function labelWrapText() as object
+function labelWrapText(includeAllLines=false as boolean) as object
     contentArea = m.GetContentArea()
     lines = []
     lineNum = 0
-    maxLines = int(contentArea.height / m.font.GetOneLineHeight())
+    if includeAllLines then
+        maxLines = invalid
+    else
+        maxLines = int(contentArea.height / m.font.GetOneLineHeight())
+    end if
 
     startPos = 0
 
-    while lines.Count() < maxLines and startPos < m.text.len()
+    while (maxLines <> invalid and lines.Count() < maxLines and startPos < m.text.len())  or (startPos < m.text.len())
         ' If this is the last allowed line, then just truncate the string
-        if lines.Count() = maxLines-1 then
+        if maxLines <> invalid and lines.Count() = maxLines-1 then
             lines.Push(m.TruncateText(m.text.Mid(startPos)))
         else
             ' Try to break on spaces. If the first whole word won't fit,
@@ -159,6 +164,11 @@ function labelWrapText() as object
     end while
 
     return lines
+end function
+
+function labelGetAllLines(width=invalid as dynamic) as object
+    if width <> invalid then m.width = width
+    return m.WrapText(true)
 end function
 
 function labelTruncateText(fullText=invalid as dynamic) as string

Plex2d/source/components/Box.brs
@@ -69,7 +69,7 @@ function boxCalculateOffsets(totalSize as integer, initialOffset as integer, siz
                 usedExtraSpace = usedExtraSpace + extraSpace
                 index = index + 1
             next
-        else if totalPreferredSize > availableSize and m.scrollHeight = invalid then
+        else if totalPreferredSize > availableSize and not m.isVScrollable = true then
             ' We need to shrink things to fit.
             cumulativeSize = 0
             index = 1

Plex2d/source/components/Component.brs
@@ -91,7 +91,6 @@ end sub
 
 sub compInitRegion()
     perfTimer().mark()
-    ' performance++ clear and resuse a region
     if m.region <> invalid then
         m.region.clear(m.bgColor)
         msg = "clear and reuse"
@@ -101,7 +100,6 @@ sub compInitRegion()
         m.region = CreateObject("roRegion", bmp, 0, 0, bmp.GetWidth(), bmp.GetHeight())
         msg = "new bitmap/region"
     end if
-
     PerfTimer().Log("compInitRegion:: " + msg + " " + tostr(m.region.getWidth()) + "x" + tostr(m.region.getHeight()))
 end sub
 
@@ -149,8 +147,7 @@ function compGetContentArea() as object
 end function
 
 sub compSetFrame(x as integer, y as integer, width as integer, height as integer)
-    m.x = x
-    m.y = y
+    m.SetPosition(x, y)
     m.width = width
     m.height = height
     ' move the sprite components sprite if applicable
@@ -162,6 +159,8 @@ end sub
 sub compSetPosition(x as integer, y as integer)
     m.x = x
     m.y = y
+    m.origX = x
+    m.origY = y
 end sub
 
 sub compSetVisible(visible=true as boolean)
@@ -207,15 +206,21 @@ end sub
 
 sub compShiftPosition(deltaX=0 as integer, deltaY=0 as integer, shiftSprite=true as boolean)
     ' ignore shifting fixed components
-    if m.fixed = true then return
+    if m.fixed = true or (m.fixedVertical = true and m.fixedHorizontal = true) then return
 
-    ' ignore vertical shift, if vertically fixed (allow horizontal shift if supplied)
+    ' ignore vertical shift / allow horizontal shift
     if m.fixedVertical = true then
         if deltaX = 0 then return
         deltaY = 0
     end if
 
-    ' set the componets new coords
+    ' ignore horizontal shift / allow vertical shift
+    if m.fixedHorizontal = true then
+        if deltaY = 0 then return
+        deltaX = 0
+    end if
+
+    ' set the components new coords
     m.x = m.x + deltaX
     m.y = m.y + deltaY
 
@@ -230,7 +235,8 @@ sub compShiftPosition(deltaX=0 as integer, deltaY=0 as integer, shiftSprite=true
     if m.sprite <> invalid then
         ' Ignore setting zOrder for scrollable parents. e.g. VBOX vertical
         ' scroll. Just move the sprite as the parent handles visibility.
-        if m.parent <> invalid and m.parent.scrollHeight <> invalid then
+        parent = firstOf(m.shiftableParent, m.parent)
+        if parent <> invalid and parent.isVScrollable = true then
             m.sprite.moveTo(m.x, m.y)
         else if m.IsOnScreen() then
             m.sprite.moveTo(m.x, m.y)

Plex2d/source/components/Container.brs
@@ -54,10 +54,10 @@ function contDraw() as object
             childRegions = component.Draw()
             for each region in childRegions
                 ' containers of containers! we'll need to check the vertical scrolling
-                ' contraints here to override the zOrder for the sprite creation.
-                if component.scrollHeight <> invalid and component.scrollVisible = false and (region.y < component.y or region.y + region.height > component.scrollHeight) then
+                ' constraints here to override the zOrder for the sprite creation.
+                if component.isVScrollable = true and component.scrollVisible = false and (region.y < component.y or region.y + region.height > component.scrollHeight) then
                     region.zOrderInit = -1
-                else if m.scrollHeight <> invalid and m.scrollVisible = false and (region.y < m.y or region.y + region.height > m.scrollHeight) then
+                else if m.isVScrollable <> invalid and m.scrollVisible = false and (region.y < m.y or region.y + region.height > m.scrollHeight) then
                     region.zOrderInit = -1
                 end if
                 regions.Push(region)
@@ -95,6 +95,8 @@ sub contSetPosition(x as integer, y as integer)
 
         m.x = x
         m.y = y
+        m.origX = x
+        m.origY = y
 
         ' If we need to reperform layout anyway, then there's no value in
         ' updating the position of our children.

Plex2d/source/components/ScrollBar.brs
@@ -1,53 +1,0 @@
-function ScrollbarClass()
-    if m.ScrollbarClass = invalid then
-        obj = CreateObject("roAssociativeArray")
-        obj.ClassName = "Scrollbar"
-
-        obj.Move = scrollbarMove
-        obj.Show = scrollbarShow
-        obj.Hide = scrollbarHide
-
-        m.ScrollbarClass = obj
-    end if
-
-    return m.ScrollbarClass
-end function
-
-function createScrollbar(yOffset as object, contentHeight as integer, containerHeight as integer, visZOrder=999 as integer) as object
-    ' TODO(rob): use a custom image, or some better non blocky scrollbar
-    obj = createBlock(&hffffff30)
-    obj.Append(ScrollbarClass())
-
-    obj.yOffset = yOffset
-    obj.contentHeight = contentHeight - yOffset
-    obj.containerHeight = containerHeight - yOffset
-    obj.height = obj.contentHeight * obj.contentHeight / obj.containerHeight
-    obj.zOrderInit = -1
-    obj.visZOrder = visZOrder
-
-    return obj
-end function
-
-sub scrollbarMove(toFocus as object, delta=0 as integer)
-    if m.delta = invalid then m.delta = 0
-    m.delta = m.delta + delta
-
-    rect = computeRect(toFocus)
-    focusY = rect.up + rect.height/2 - m.yOffset + m.delta*-1
-    m.y = (focusY / m.containerHeight) * (m.contentHeight - m.height) + m.yOffset
-    if m.sprite <> invalid then
-        m.sprite.moveTo(m.x, m.y)
-    end if
-end sub
-
-sub scrollbarHide()
-    if m.sprite <> invalid then
-        m.sprite.setZ(-1)
-    end if
-end sub
-
-sub scrollbarShow()
-    if m.sprite <> invalid then
-        m.sprite.setZ(m.visZOrder)
-    end if
-end sub

Plex2d/source/components/Scrollbar.brs
@@ -1,0 +1,78 @@
+function ScrollbarClass()
+    if m.ScrollbarClass = invalid then
+        obj = CreateObject("roAssociativeArray")
+        obj.Append(ComponentClass())
+        obj.ClassName = "Scrollbar"
+
+        obj.Show = scrollbarShow
+        obj.Draw = scrollbarDraw
+        obj.Move = scrollbarMove
+        obj.Hide = scrollbarHide
+
+        m.ScrollbarClass = obj
+    end if
+
+    return m.ScrollbarClass
+end function
+
+function createScrollbar(yOffset as object, contentHeight as integer, containerHeight as integer, zOrder=ZOrders().SCROLLBAR as integer, offset=invalid as dynamic) as object
+    obj = CreateObject("roAssociativeArray")
+    obj.Append(ScrollbarClass())
+
+    obj.Init()
+
+    obj.fgColor = Colors().ScrollbarFg
+    obj.bgColor = Colors().ScrollbarBg
+
+    obj.yOffset = yOffset
+    obj.contentHeight = contentHeight - yOffset
+    obj.containerHeight = containerHeight - yOffset
+    obj.scrollbarHeight = obj.contentHeight * obj.contentHeight / obj.containerHeight
+    obj.height = obj.contentHeight
+
+    obj.scrollbarY = 0
+    obj.offset = iif(offset = invalid, 0, offset)
+    obj.zOrder = zOrder
+    obj.zOrderInit = -1
+
+    ' Default shifting rules
+    obj.fixed = true
+    obj.fixedVertical = true
+    obj.fixedHorizontal = true
+
+    return obj
+end function
+
+function scrollbarDraw() as object
+    ApplyFunc(ComponentClass().Draw, m)
+    m.region.DrawRect(0, m.scrollbarY, m.width, m.scrollbarHeight, m.fgColor)
+
+    return [m]
+end function
+
+sub scrollbarMove(toFocus as object, isFirst=false as boolean, isLast=false as boolean)
+    if m.sprite = invalid then return
+
+    if isFirst then
+        m.scrollbarY = 0
+    else if isLast then
+        m.scrollbarY = m.contentHeight - m.scrollbarHeight
+    else
+        focusY = toFocus.origY + (toFocus.height/2) - m.yOffset
+        m.scrollbarY = int((focusY / (m.containerHeight-m.offset)) * (m.contentHeight - m.scrollbarHeight))
+    end if
+
+    m.Draw()
+end sub
+
+sub scrollbarHide()
+    if m.sprite <> invalid then
+        m.sprite.setZ(-1)
+    end if
+end sub
+
+sub scrollbarShow()
+    if m.sprite <> invalid then
+        m.sprite.setZ(m.zOrder)
+    end if
+end sub

Plex2d/source/components/TextArea.brs
@@ -1,0 +1,204 @@
+function TextAreaClass() as object
+    if m.TextAreaClass = invalid then
+        obj = CreateObject("roAssociativeArray")
+        obj.Append(AlignmentMixin())
+        obj.Append(VBoxClass())
+        obj.ClassName = "TextArea"
+
+        ' Methods
+        obj.Init = textareaInit
+        obj.PerformLayout = textareaPerformLayout
+        obj.SetVisible = textareaSetVisible
+        obj.SetPadding = textareaSetPadding
+        obj.SetColor = textareaSetColor
+
+        m.TextAreaClass = obj
+    end if
+
+    return m.TextAreaClass
+end function
+
+function createTextArea(text as string, font as object, spacing=0 as integer, scrollPos="right" as string) as object
+    obj = CreateObject("roAssociativeArray")
+    obj.Append(TextAreaClass())
+
+    obj.Init()
+
+    obj.text = text
+    obj.font = font
+    obj.spacing = spacing
+
+    obj.SetScrollable(obj.font.GetOneLineHeight(), false, false, scrollPos)
+
+    return obj
+end function
+
+sub textareaInit()
+    ApplyFunc(VBoxClass().Init, m)
+
+    ' Default VBox methods
+    m.homogeneous = false
+    m.expand = false
+    m.fill = false
+
+   ' Override default colors
+    m.fgColor = invalid
+    m.bgColor = invalid
+    m.bgColorFocus = invalid
+
+    ' Default shifting rules
+    m.stopShiftIfInView = true
+    m.fixedHorizontal = true
+
+    ' Text padding
+    m.textPadding = {
+        marginBottom: 0,
+        marginTop: 0,
+        width: 0,
+    }
+end sub
+
+sub textareaPerformLayout()
+    m.DestroyComponents()
+    zOrder = iif(m.zOrder <> invalid and m.zOrder > 1, m.zOrder, 2)
+
+    ' Reset the yOffset and Height based on the padding prefs (margins)
+    m.y = m.y + m.textPadding.marginTop
+    m.height = m.height - m.textPadding.marginBottom
+
+    for each line in createLabel(m.text, m.font).GetAllLines(m.width - m.textPadding.width)
+        label = createLabel(line, m.font)
+        label.width = m.width
+        label.halign = m.halign
+        label.fixed = false
+        label.fixedHorizontal = (m.fixedHorizontal = true)
+        label.focusBorder = false
+        label.zOrder = zOrder
+        label.zOrderInit = m.zOrderInit
+
+        label.SetFocusable(invalid, true)
+        label.SetPadding(0, m.textPadding.right, 0, m.textPadding.left)
+        if m.fgColor <> invalid then
+            label.SetColor(m.fgColor)
+        end if
+
+        ' Methods to focus the text area. Always focus the first item,
+        ' and maintain current focus of the text area ongoing.
+        label.GetFocusManual = textareaGetFocusManual
+        label.OnFocus = textareaOnFocus
+        label.OnBlur = textareaOnBlur
+        label.isTextArea = true
+        if m.firstFocusItem = invalid then m.firstFocusItem = label
+
+        m.AddComponent(label)
+    end for
+
+    ' Perform a VBox layout when we have all of our components
+    ApplyFunc(VBoxClass().PerformLayout, m)
+
+    ' Add a background component if we a bgColor
+    if m.bgColor <> invalid then
+        m.background = createBlock(m.bgColor)
+        m.background.SetFrame(m.x, m.y - m.textPadding.marginTop, m.width, m.height + m.textPadding.marginBottom)
+        m.background.zOrder = zOrder - 1
+        m.background.zOrderInit = m.zOrderInit
+
+        ' allow the background to shift horizontally if applicable
+        m.background.fixed = false
+        m.background.fixedHorizontal = (m.fixedHorizontal = true)
+        m.background.fixedVertical = true
+
+        m.components.Unshift(m.background)
+    end if
+
+    if m.components.Count() <= 1 then return
+
+    ' Update which lines are focusable. This will stop scrolling
+    ' when the final components are all inve view
+    lastScrollInView = m.containerHeight - m.contentHeight + m.y
+
+    m.components.Reset()
+    while m.components.IsNext()
+        component = m.components.Next()
+        if m.containerHeight <= m.contentHeight or component.origY >= lastScrollInView + component.height then
+            component.SetFocusable(invalid, false)
+        end if
+    end while
+end sub
+
+sub textareaSetPadding(pTop as integer, pRight=invalid as dynamic, pBottom=invalid as dynamic, pLeft=invalid as dynamic)
+    pad = m.textPadding
+
+    ApplyFunc(PaddingMixin().SetPadding, pad, [pTop, pRight, pBottom, pLeft])
+
+    pad.left = pad.padding.left
+    pad.right = pad.padding.right
+    pad.width = pad.left + pad.right
+
+    if pad.padding.top > 0 then
+        pad.marginTop =  pad.padding.top
+        pad.marginBottom =  pad.padding.top
+    end if
+
+    if pad.padding.bottom > 0 then
+        pad.marginBottom = pad.marginBottom + pad.padding.bottom
+    end if
+end sub
+
+sub textareaSetVisible(visible=true as boolean)
+    ApplyFunc(VBoxClass().SetVisible, m, [visible])
+
+    ' Due to padding, the background may be outside the visible constraints, so
+    ' we'll set it visiable regardless.
+    if m.background <> invalid then m.background.SetVisible(visible)
+
+    if m.visible = visible then return
+    m.visible = visible
+
+    ' Toggle focus based on visibility
+    m.components.Reset()
+    while m.components.IsNext()
+        component = m.components.Next()
+        if not visible and component.focusable = true then
+            component.SetFocusable(invalid, visible)
+            component.wasFocusable = true
+        else if visible and component.wasFocusable = true then
+            component.SetFocusable(invalid, visible)
+        end if
+    end while
+end sub
+
+function textareaGetFocusManual() as dynamic
+    return m.parent.firstFocusItem
+end function
+
+sub textareaOnFocus()
+    m.parent.firstFocusItem = m
+
+    if m.parent.isFocused = true then return
+    m.parent.isFocused = true
+
+    if m.parent.background <> invalid then
+        m.parent.background.bgColor = firstOf(m.parent.bgColorFocus, m.parent.bgColor)
+        m.parent.background.Draw()
+        m.parent.background.SetVisible(true)
+    end if
+end sub
+
+sub textareaOnBlur(toFocus as object)
+    if toFocus.isTextArea = true then return
+
+    if m.parent.isFocused = false then return
+    m.parent.isFocused = false
+
+    if m.parent.background <> invalid then
+        m.parent.background.bgColor = m.parent.bgColor
+        m.parent.background.Draw()
+    end if
+end sub
+
+sub textareaSetColor(fgColor as integer, bgColor=invalid as dynamic, bgColorFocus=invalid)
+    m.fgColor = fgColor
+    m.bgColor = bgColor
+    m.bgColorFocus = bgColorFocus
+end sub

Plex2d/source/components/VBox.brs
@@ -12,6 +12,7 @@ function VBoxClass() as object
         obj.GetPreferredHeight = vboxGetPreferredHeight
         obj.AddSpacer = vboxAddSpacer
         obj.SetScrollable = vboxSetScrollable
+        obj.SetVisible = vboxSetVisible
 
         m.VBoxClass = obj
     end if
@@ -39,6 +40,8 @@ end sub
 
 sub vboxPerformLayout()
     m.needsLayout = false
+    m.scrollHeight = m.height + m.y
+
     numChildren = m.components.Count()
 
     ' Strange, but let's not even bother with the complicated stuff if we don't need to.
@@ -73,31 +76,44 @@ sub vboxPerformLayout()
         xOffset = m.GetXOffsetAlignment(m.x, m.width, width, firstOf(component.phalign, component.halign))
         component.SetFrame(xOffset, offset, width, height)
 
-        ' retain information about the scrollable container to add scrollbar and/or change opacity on scroll
+        ' calculate the container height, content height and other data about the scrollable area
         if m.scrollTriggerDown <> invalid then
-            if m.scrollInfo = invalid then m.scrollInfo = { zOrder: 2, contentHeight: m.scrollTriggerDown }
+            if m.scrollInfo = invalid then m.scrollInfo = { zOrder: 2 }
+            if m.origScrollTriggerDown = invalid then m.origScrollTriggerDown = m.scrollTriggerDown
+            if component.focusInside = true then m.scrollInfo.focusInside = true
 
-            if component.focusInside = true then
-                m.scrollInfo.focusInside = true
+            ' height of all components in the container
+            m.containerHeight = offset + height
+            m.lastShift = m.containerHeight
+
+            ' calculate the exact content height that fits within m.scrollTriggerDown
+            if m.containerHeight <= m.origScrollTriggerDown then
+                m.scrollTriggerDown = m.containerHeight
             end if
 
-            ' calculate the exact content heigh that fits within m.scrollTriggerDown
-            m.scrollInfo.containerHeight = offset + height
-            if m.scrollInfo.containerHeight <= m.scrollTriggerDown then
-                m.scrollInfo.contentHeight = m.scrollInfo.containerHeight
+            ' calculate the content height that fits in wanted container height
+            if m.containerHeight <= offsets[0] + m.height then
+                m.contentHeight = m.containerHeight
             end if
 
-            ' determine the zOrder required for any additional components
+            ' determine the zOrder required for any additional components (scrollbar/opacity blocks)
             if component.zOrder <> invalid and component.zOrder > m.scrollInfo.zOrder then
                 m.scrollInfo.zOrder = component.zOrder + 1
             end if
         end if
     end while
 
-    ' scrollbar and opacity helpers for scrollable containers
+    ' scrolling helpers: scrollbar, opacity blocks, stop shifting, stop focusing, etc..
     if m.scrollInfo <> invalid then
+        ' disable any further shift if we want to stop when the last components
+        ' are in view. We might want to make this a default.
+        if m.stopShiftIfInView = true then
+            m.lastShiftInView = m.containerHeight - m.contentHeight + offsets[0]
+            m.scrollInfo.offsetContainer = m.containerHeight - m.lastShiftInView
+        end if
+
         ' disallow manual focus DOWN for scrolling containers
-        if m.scrollInfo.containerHeight > m.scrollInfo.contentHeight then
+        if m.containerHeight > m.contentHeight then
             m.disallowExit = { down: true }
         end if
 
@@ -116,19 +132,17 @@ sub vboxPerformLayout()
             opacityBot.zOrder = m.scrollInfo.zOrder
             opacityBot.fixed = false
             opacityBot.fixedVertical = true
-            opacityBot.setFrame(xOffset, m.scrollInfo.contentHeight, m.width, 720-m.scrollInfo.contentHeight)
+            opacityBot.setFrame(xOffset, m.contentHeight, m.width, 720 - m.contentHeight)
 
             m.components.push(opacityTop)
             m.components.push(opacityBot)
         end if
 
         ' add a scrollbar
-        if m.scrollbarPos <> invalid and m.scrollInfo.containerHeight > m.scrollInfo.contentHeight then
-            ' disallow manual focus DOWN for scrolling containers
-            m.disallowExit = { down: true }
-            yOffset = offsets[0]
-            scrollbar = createScrollBar(yOffset, m.scrollInfo.contentHeight, m.scrollInfo.containerHeight, m.scrollInfo.zOrder)
-            if scrollbar <> invalid
+        if m.scrollbarPos <> invalid and m.containerHeight > m.contentHeight then
+            contentHeight = iif(m.stopShiftIfInView = true, m.scrollHeight, m.contentHeight)
+            m.scrollbar = createScrollbar(offsets[0], contentHeight, m.containerHeight, m.scrollInfo.zOrder, m.scrollInfo.offsetContainer)
+            if m.scrollbar <> invalid
                 width = int(CompositorScreen().focusPixels * 1.5)
                 spacing = iif(m.scrollInfo.focusInside = true, CompositorScreen().focusPixels, CompositorScreen().focusPixels*2)
                 if m.scrollbarPos = "right" then
@@ -136,10 +150,13 @@ sub vboxPerformLayout()
                 else
                     xOffset = xOffset - width - spacing
                 end if
-                scrollbar.setFrame(xOffset, yOffset, width, scrollbar.height)
+                m.scrollbar.setFrame(xOffset, offsets[0], width, m.scrollbar.height)
+                if m.fixedHorizontal = false then
+                    m.scrollbar.fixedHorizontal = false
+                    m.scrollbar.fixed = false
+                end if
 
-                m.components.push(scrollBar)
-                m.scrollbar = scrollbar
+                m.AddComponent(m.scrollbar)
             end if
         end if
 
@@ -205,12 +222,22 @@ sub vboxCalculateShift(toFocus as object, refocus=invalid as dynamic)
         shift.y = shift.hideUp - focusRect.up
     end if
 
+    if m.isVScrollable <> invalid then
+        if toFocus.origY > firstOf(m.lastShiftInView, m.lastShift) then
+            shift.y = 0
+        end if
+
+        ' shift the scrollbar if applicable
+        if m.scrollbar <> invalid then
+            isLast = (iif(m.lastShiftInView = invalid, toFocus.origY + toFocus.height, toFocus.origY) >= m.lastShift)
+            isFirst = (toFocus.origY = m.y)
+            m.scrollbar.Move(toFocus, isFirst, isLast)
+        end if
+    end if
+
     if shift.y <> 0 then
         m.shiftComponents(shift)
     end if
-
-    ' shift the scrollbar if applicable
-    if m.scrollBar <> invalid then m.scrollBar.Move(toFocus, shift.y)
 end sub
 
 sub vboxShiftComponents(shift)
@@ -226,20 +253,12 @@ sub vboxShiftComponents(shift)
         end for
     end if
 
-    if m.scrollVisible = true then
-        shift.hideUp = invalid
-        shift.hideDown = invalid
-    end if
-
-    ' set the visibility based on the constraints
-    for each comp in m.components
-        comp.SetVisibility(invalid, invalid, shift.hideUp, shift.hideDown)
-    end for
+    m.SetVisible()
 end sub
 
-sub vboxSetScrollable(scrollHeight as integer, scrollTriggerDown=invalid as dynamic, scrollAnimate=false as boolean, scrollVisible=false as boolean, scrollbarPos="right" as dynamic)
-    m.scrollHeight = scrollHeight
-    m.scrollTriggerDown = firstOf(scrollTriggerDown, scrollHeight)
+sub vboxSetScrollable(scrollTriggerDown=invalid as dynamic, scrollAnimate=false as boolean, scrollVisible=false as boolean, scrollbarPos="right" as dynamic)
+    m.isVScrollable = true
+    m.scrollTriggerDown = firstOf(scrollTriggerDown, m.height + m.y)
     m.scrollAnimate = scrollAnimate
     m.scrollVisible = scrollVisible
     m.scrollbarPos = scrollbarPos
@@ -247,4 +266,19 @@ sub vboxSetScrollable(scrollHeight as integer, scrollTriggerDown=invalid as dyna
     ' methods
     m.ShiftComponents = vboxShiftComponents
     m.CalculateShift = vboxCalculateShift
+end sub
+
+sub vboxSetVisible(visible=true as boolean)
+    if visible = false then
+        ApplyFunc(BoxClass().SetVisible, m, [false])
+    else if m.scrollVisible = true then
+        ApplyFunc(BoxClass().SetVisible, m, [true])
+    else
+        hide = {up: m.y, down: m.scrollHeight}
+
+        ' set the visibility based on the constraints
+        for each comp in m.components
+            comp.SetVisibility(invalid, invalid, hide.up, hide.down)
+        end for
+    end if
 end sub

commit 6c1157c84c5378d666209cc5c841c420b8a3cea5

Fix possible duplicate lines while wrapping text. #203
regression from 1e3e51f7351028fe4bc2baa2172854ae25d77298

Plex2d/source/components/Label.brs
@@ -132,7 +132,7 @@ function labelWrapText(includeAllLines=false as boolean) as object
 
     startPos = 0
 
-    while (maxLines <> invalid and lines.Count() < maxLines and startPos < m.text.len())  or (startPos < m.text.len())
+    while (maxLines <> invalid and lines.Count() < maxLines and startPos < m.text.len())  or (maxLines = invalid and startPos < m.text.len())
         ' If this is the last allowed line, then just truncate the string
         if maxLines <> invalid and lines.Count() = maxLines-1 then
             lines.Push(m.TruncateText(m.text.Mid(startPos)))